45b8755861d3715bf278dd186e4b3e6760bd5aee
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-09-26 16:30:33 +0200

    ios part working

--- rust_part/target/x86_64-apple-ios/release/deps/rust_part.d
+++ rust_part/target/x86_64-apple-ios/release/deps/rust_part.d
@@ -1,6 +1,8 @@
-/Users/justynagreda/Documents/flutter_rust_plugin/rust_part/target/x86_64-apple-ios/release/deps/librust_part.a: src/lib.rs src/api.rs
+/Users/justynagreda/Documents/flutter_rust_plugin/rust_part/target/x86_64-apple-ios/release/deps/librust_part.a: src/lib.rs src/bridge_generated.rs src/bridge_generated.io.rs src/api.rs
 
-/Users/justynagreda/Documents/flutter_rust_plugin/rust_part/target/x86_64-apple-ios/release/deps/rust_part.d: src/lib.rs src/api.rs
+/Users/justynagreda/Documents/flutter_rust_plugin/rust_part/target/x86_64-apple-ios/release/deps/rust_part.d: src/lib.rs src/bridge_generated.rs src/bridge_generated.io.rs src/api.rs
 
 src/lib.rs:
+src/bridge_generated.rs:
+src/bridge_generated.io.rs:
 src/api.rs:
