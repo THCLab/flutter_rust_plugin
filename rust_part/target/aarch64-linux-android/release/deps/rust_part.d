3a39ce7c5216c6fbcfe8f83d5f6839b0816f442b
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-09-26 16:19:48 +0200

    android part working

--- rust_part/target/aarch64-linux-android/release/deps/rust_part.d
+++ rust_part/target/aarch64-linux-android/release/deps/rust_part.d
@@ -1,8 +1,10 @@
-C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\librust_part.a: src\lib.rs src\api.rs
+C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\librust_part.a: src\lib.rs src\bridge_generated.rs src\bridge_generated.io.rs src\api.rs
 
-C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\librust_part.so: src\lib.rs src\api.rs
+C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\librust_part.so: src\lib.rs src\bridge_generated.rs src\bridge_generated.io.rs src\api.rs
 
-C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\rust_part.d: src\lib.rs src\api.rs
+C:\Users\66jus\Documents\GitHub\flutter_rust_plugin\rust_part\target\aarch64-linux-android\release\deps\rust_part.d: src\lib.rs src\bridge_generated.rs src\bridge_generated.io.rs src\api.rs
 
 src\lib.rs:
+src\bridge_generated.rs:
+src\bridge_generated.io.rs:
 src\api.rs:
