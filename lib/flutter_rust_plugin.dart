936633092634b7aa7fe7a1f46782e301787b4398
gredajustyna <83274413+gredajustyna@users.noreply.github.com>
2022-09-26 16:13:18 +0200

    windows part working

--- lib/flutter_rust_plugin.dart
+++ lib/flutter_rust_plugin.dart
@@ -6,12 +6,12 @@ import 'package:flutter_rust_plugin/bridge_generated.dart';
 class FlutterRustPlugin {
   static const base = 'rust_part';
   static final path = Platform.isWindows? '$base.dll' : 'lib$base.so';
-  static late final dylib = Platform.isIOS
+  static final dylib = Platform.isIOS
       ? DynamicLibrary.process()
       : Platform.isMacOS
       ? DynamicLibrary.executable()
       : DynamicLibrary.open(path);
-  static late final api = RustPartImpl(dylib);
+  static final api = RustPartImpl(dylib);
 
   static Future<String> hello() async{
     var hello = await api.hello();
